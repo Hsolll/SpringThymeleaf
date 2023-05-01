e7b1a1fec02fcbc5e7bf49551031cab8e22d63c7
Hsolll <doc9345@naver.com>
2023-05-01 23:54:11 +0900

    create project

---
+++ src/main/java/com/spring/websiteProject/WebsiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.websiteProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/websiteProject/WebsiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.websiteProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
